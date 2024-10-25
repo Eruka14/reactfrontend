b4f3c9dcb0840b043844b19bbcfd373341ca8580
Fyodor <shobshob481@gmail.com>
2024-10-26 03:56:24 +0800

    bug fixed

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -5,7 +5,10 @@ const Analytics = () => {
   return (
     <div className="w-full bg-white py-10">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center items-center md:flex-row gap-4">
-        <img src={laptop} className="flex-1 max-w-[400px] md:max-w-[500px]" />
+        <img
+          src={laptop}
+          className="flex-1 max-w-[400px] max-h-[400px] md:max-w-[500px] md:max-h-[500px]"
+        />
         <div className="flex-1 flex flex-col items-start text-black gap-3 p-3">
           <h3 className="text-md md:text-xl font-bold text-green-400">
             Өгөгдлийн хянах самбар
